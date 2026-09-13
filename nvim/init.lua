20904f84a50dd169d80368177390ae331471a229
Niumlaque <niumlaque@gmail.com>
2026-09-13 22:34:24 +0900

    Add the Molokai color scheme to the Neovim configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,7 +5,10 @@ local Plug = vim.fn["plug#"]
 vim.call("plug#begin")
 Plug("nvim-lua/plenary.nvim")
 Plug("nvim-telescope/telescope.nvim")
+Plug("tomasr/molokai")
 vim.call("plug#end")
+
+vim.cmd.colorscheme("molokai")
 
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>b", "<cmd>Telescope buffers<CR>")
